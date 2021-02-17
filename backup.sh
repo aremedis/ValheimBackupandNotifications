6482f73e86bb9c51741c7745f2884d2aa3f1f598
Robert W <rwatson@atlassian.com>
2021-02-17 11:43:52 -0600

    basic script done

--- backup.sh
+++ backup.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/bash
 now=$(date +"%m-%d-%Y")
+webhook="NULL"
 
+# Sending Stopping notification
+message1 = "Valheim Server stopping in 5 minutes."
+message2 = "Valheim Server has been restarted."
+/home/ubuntu/Scripts/BashDiscord/discord.sh --webhook=$webhook --username "Odin" --text $message1
 
+sleep 5m
 
 # Stop the Service
 systemctl stop valheimserver.service
@@ -13,3 +19,7 @@ cp /home/steam/.config/unity3d/IronGate/Valheim/worlds/Charlie.fwl /home/steam/W
 
 
 # Start the Service
+systemctl start valheimserver.service
+
+sleep 5m
+/home/ubuntu/Scripts/BashDiscord/discord.sh --webhook=$webhook --username "Odin" --text $message2
